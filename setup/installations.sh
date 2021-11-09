043a399817d4a31dd8aeee69dec0ffccdaa23d56
Guilherme Palazzo <35868223+gpalazzo@users.noreply.github.com>
2021-11-08 21:07:45 -0300

    Feature/add setup scripts (#4)
    * changing setup scripts
    
    * deleting file wrongly commited

--- setup/installations.sh
+++ setup/installations.sh
@@ -1,16 +1,25 @@
 # base
 sudo passwd ubuntu #asks for a new password to execute su commands
 
-# technologies already installed by default: git and python
+# technologies already installed by default: git, vim
 
 # update
 sudo apt update
+
+# base
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+
+sudo apt update
+
+# install python
+sudo apt-get install python3.7
 
 # miniconda for virtual env
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh && rm -rf ./Miniconda3-latest-Linux-x86_64.sh
-export PATH="/home/ubuntu/miniconda3/bin:$PATH" >> ~/.bashrc
+echo 'export PATH="/home/ubuntu/miniconda3/bin:$PATH"' >> ~/.bashrc
 
 # postgres database
 sudo apt install postgresql postgresql-contrib
@@ -27,3 +36,10 @@ sudo usermod -aG docker ${USER} #add current logged user to docker group to avoi
 su - ${USER}
 id -nG #verify the user was added
 
+# docker-compose v1.29.2
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# htop
+sudo snap install htop
