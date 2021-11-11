4189927adf814c1c2cd38de58086b359cc45202b
Guilherme Palazzo <palazzoeng1@gmail.com>
2021-11-11 16:22:26 +0000

    adding airflow dummy data collection

--- po201_etl/src/data_domains/dummy/raw/get_data.py
+++ po201_etl/src/data_domains/dummy/raw/get_data.py
@@ -1,5 +1,4 @@
 import yfinance as yf
-import yahoo_fin.stock_info as si
 import pandas as pd
 import requests
 from typing import List
@@ -29,65 +28,6 @@ def parse_stocks_index(target_index: str = "IBXX.SA") -> List[str]:
     return list(set(df["Symbol"]))
 
 
-#
-#
-# # BRAX11 é o ETF que replica a performance do IBrX-100: índice com as 100 ações mais líquidas da Bovespa
-# mkt_index = "BRAX11.SA"
-top30_stocks = parse_stocks_index()
-#
-# # MARKET INDEX CLOSE PRICE DATA
-# mkt_index_close_price = yf.download(tickers=mkt_index, period="max", interval="1d")[
-#     "Adj Close"
-# ].reset_index()
-#
-# mkt_index_close_price.to_csv("../data/raw/mkt_index_price_brax11.csv", index=False)
-#
-# # TOP 30 IBXX (IBRX-100) STOCKS CLOSE PRICE DATA
-# index_min_date = mkt_index_close_price["Date"].min()
-#
-# tickers_close_prices = yf.download(tickers=top30_stocks, period="max", interval="1d")[
-#     "Adj Close"
-# ].reset_index()
-#
-# tickers_close_prices = tickers_close_prices[
-#     tickers_close_prices["Date"] >= index_min_date
-# ]
-#
-# tickers_close_prices.to_csv(
-#     "../data/raw/top30_stocks_price_within_brax11.csv", index=False
-# )
-#
-# # TOP 30 IBXX (IBRX-100) STOCKS MARKET CAPITALIZATION DATA
-# mkt_cap_dict = {}
-# for ticker in top30_stocks:
-#     print(f"Getting market cap data for: {ticker}...")
-#     stock = yf.Ticker(ticker)
-#     mkt_cap_dict[ticker] = stock.info["marketCap"]
-#
-#
-# mkt_cap_30stocks = (
-#     pd.DataFrame.from_dict(mkt_cap_dict, orient="index")
-#     .reset_index()
-#     .rename(columns={0: "mkt_cap", "index": "ticker"})
-# )
-#
-# mkt_cap_30stocks.to_csv(
-#     "../data/raw/top30_stocks_mktcap_within_brax11.csv", index=False
-# )
-
-# DADOS PARA A REGRESSÃO MÚLTIPLA
-# tickers = ["USDBRL=X"]
-# df = yf.download(tickers=tickers, period="max", interval="1d")[
-#     "Adj Close"
-# ].reset_index()
-# df.to_csv("../data/raw/usdbrl_fx.csv", index=False)
-
-# income_dict = {}
-for ticker in top30_stocks:
-    df_income = si.get_income_statement(ticker=ticker)
-    df_transposed = df_income.transpose()
-    df_parsed = df_transposed[["grossProfit"]]
-
-
-print(df_parsed)
+df = yf.download("AAPL", start="2020-02-01", end="2020-03-20")["Adj Close"].reset_index()
+print(df)
 
