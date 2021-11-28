55f48a06cfe4c785c7ead29a6aef83eaa33369e3
Guilherme Palazzo <palazzoeng1@gmail.com>
2021-11-28 14:52:47 -0300

    improving raw layer

--- po201_etl/src/data_domains/yahoo_finance/raw/yahoo_finance_raw.py
+++ po201_etl/src/data_domains/yahoo_finance/raw/yahoo_finance_raw.py
@@ -1,6 +1,7 @@
 from utils import dump_data_pgsql, read_data_pgsql, load_and_merge_ymls
 import yfinance as yf
 import os
+import time
 
 
 CONFIG_PATH = [f"{os.getenv('PROJECT_ROOT_PATH')}/conf/yahoo_finance/io.yml"]
@@ -13,8 +14,17 @@ stocks = df["stocks_name"].unique().tolist()
 
 
 for i, stock in enumerate(stocks, 1):
+
+    time.sleep(2)
+
     print(f"Parsing data for stock: {stock}")
     print(f"Stock {i} out of {len(stocks)} stocks")
     df = yf.download(stock, period="max")["Adj Close"].reset_index()
-    df.loc[:, "yf_stock_name"] = stock
-    dump_data_pgsql(df=df, database=config["yf_raw_db_name"], yf_stock_name=stock)
+
+    if df.empty:
+        print(f"Skipping stock: {stock} because it has no data")
+        continue
+
+    else:
+        df.loc[:, "yf_stock_name"] = stock
+        dump_data_pgsql(df=df, database=config["yf_raw_db_name"], yf_stock_name=stock)
