6064dbbf6ba084ce23a2187762751bd0af815019
Guilherme Palazzo <palazzoeng1@gmail.com>
2021-11-28 16:26:50 -0300

    e2e functional kedro pipeline

--- po201_kedro_model/src/po201_kedro_model/data_domains/yahoo_finance/features/yahoo_finance_fte.py
+++ po201_kedro_model/src/po201_kedro_model/data_domains/yahoo_finance/features/yahoo_finance_fte.py
@@ -1,14 +1,48 @@
 import pandas as pd
 from typing import List
+from datetime import timedelta, datetime
 
 
 def yahoo_finance_features(
-    df: pd.DataFrame, month_roll_window: List[int]
+    df: pd.DataFrame,
+    month_roll_window: List[int],
+    days_lookback: int,
+    null_pct_cut: float,
 ) -> pd.DataFrame:
 
-    df = _calculate_rolling_windows(df=df, month_roll_window=month_roll_window)
+    df_pre_processed = _fte_pre_processing(
+        df=df, days_lookback=days_lookback, null_pct_cut=null_pct_cut
+    )
 
-    return df
+    df_ftes = _calculate_rolling_windows(
+        df=df_pre_processed, month_roll_window=month_roll_window
+    )
+
+    return df_ftes
+
+
+def _fte_pre_processing(
+    df: pd.DataFrame, days_lookback: int, null_pct_cut: float
+) -> pd.DataFrame:
+
+    lower_bound_date = datetime.today() - timedelta(days=days_lookback)
+
+    # filter date
+    df_filter_date = df[df["date"] >= lower_bound_date]
+
+    # filter null percentage
+    _mask = df_filter_date.isna().mean()
+    df_filter_null = df_filter_date.loc[:, _mask <= null_pct_cut]
+
+    # input nulls
+    #   get columns with null value
+    null_cols = df_filter_null.columns[df_filter_null.isna().any()].tolist()
+
+    #   input with linear interpolation
+    for col in null_cols:
+        df_filter_null[col] = df_filter_null[col].interpolate(method="linear")
+
+    return df_filter_null
 
 
 def _calculate_rolling_windows(
