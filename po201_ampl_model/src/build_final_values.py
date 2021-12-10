70ca161ae41047933cf02d2c3177238890926164
Guilherme Palazzo <palazzoeng1@gmail.com>
2021-12-10 11:25:00 -0300

    adding ampl reading from pgsql

--- po201_ampl_model/src/build_final_values.py
+++ po201_ampl_model/src/build_final_values.py
@@ -23,25 +23,8 @@ def read_data_pgsql(
     return df
 
 
-df = read_data_pgsql(database="aux", tbl_name="ampl_report")
-_df = df.sort_values(by="run_timestamp", ascending=False).head(1)
-_uuid = _df["uuid"].unique().tolist()[0]
-df_filter = df[df["uuid"] == _uuid]
-start_time = df_filter["run_timestamp"].min()
-end_time = df_filter["run_timestamp"].max()
-runtime = end_time - start_time
-max_return = df_filter["return"].max()
-df_final = df_filter[df_filter["return"] == max_return]
-
-breakpoint()
-
-df = read_data_pgsql(database="aux", tbl_name="ga_report")
-_df = df.sort_values(by="run_timestamp", ascending=False).head(1)
-_uuid = _df["uuid"].unique().tolist()[0]
-df_filter = df[df["uuid"] == _uuid]
-max_return = df_filter["return"].max()
-df_final = df_filter[df_filter["return"] == max_return]
-
-df2 = read_data_pgsql(database="aux", tbl_name="ga_run_time")
-df2_filter = df2[df2["uuid"] == _uuid]
+df = read_data_pgsql(database="aux", tbl_name="ampl_markowitz")
+excl_cols = ["run_timestamp", "total_run_time_min", "solve_time_min", "mean", "risk"]
+select_cols = list(set(df.columns.tolist()) - set(excl_cols))
+print(f"Soma dos pesos: {df[select_cols].sum().sum()}")
 breakpoint()
