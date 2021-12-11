86c03e06f0b43e43795a1318dca5f5cddb0727dc
Guilherme Palazzo <palazzoeng1@gmail.com>
2021-12-11 11:03:22 -0300

    adding ampl model with new data and adding report builder

--- po201_ampl_model/src/build_final_values.py
+++ po201_ampl_model/src/build_final_values.py
@@ -1,30 +1,11 @@
-import pandas as pd
-from sqlalchemy import create_engine
-import os
-
-
-def read_data_pgsql(
-    database: str, tbl_name: str = None, query: str = None
-) -> pd.DataFrame:
-
-    print(f"Reading data from PGSQL")
-    engine = create_engine(
-        f"postgresql://{os.getenv('PGSQL_USERNAME')}:{os.getenv('PGSQL_PASSWORD')}@{os.getenv('PGSQL_HOST')}:{os.getenv('PGSQL_PORT')}/{database}"
-    )
-
-    if not query:
-        df = pd.read_sql_query(
-            f'select * from {database}.public."{tbl_name}"', con=engine
-        )
-
-    else:
-        pass  # testar execução de query aqui
-
-    return df
+from utils import read_data_pgsql
 
 
 df = read_data_pgsql(database="aux", tbl_name="ampl_markowitz")
 excl_cols = ["run_timestamp", "total_run_time_min", "solve_time_min", "mean", "risk"]
 select_cols = list(set(df.columns.tolist()) - set(excl_cols))
-print(f"Soma dos pesos: {df[select_cols].sum().sum()}")
-breakpoint()
+
+print(f"Soma dos pesos: {df[select_cols].sum().sum():.2f}")
+print(f"Retorno médio: {df['mean'].unique()[0] * 100:.6f}%")
+print(f"Risco médio: {df['risk'].unique()[0]:.6f}")
+print(f"Tempo total do algoritmo: {df['total_run_time_min'].unique()[0]:.2f} minutos")
