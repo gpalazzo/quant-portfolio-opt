61e029c42de6c2964b95def0a698a34bfdcf5b77
Guilherme Palazzo <palazzoeng1@gmail.com>
2021-12-22 17:20:32 +0000

    adding flask api

---
+++ flask_api/src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restful import Api
+from resources import HealthCheck
+from waitress import serve
+
+
+# flask app
+app = Flask(__name__)
+
+# slash trailing disabled, i.e., it responds to call regardless of having a slash (/) at the end of the URL or not
+app.url_map.strict_slashes = False
+
+# api structure abstraction from app
+api = Api(app)
+
+# endpoints mapping (class name, endpoint)
+api.add_resource(HealthCheck, "/health")
+
+# serving for all calls through port 5000
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=5000)
