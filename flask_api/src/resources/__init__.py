d21879343fde26bbda53c8f859fceb22ecb63595
Guilherme Palazzo <palazzoeng1@gmail.com>
2021-12-24 09:29:13 -0300

    local changes

--- flask_api/src/resources/__init__.py
+++ flask_api/src/resources/__init__.py
@@ -1,3 +1,4 @@
 from .health import HealthCheck
 from .portfolio_optimizer import PortfolioOptimizer
 from .optimization_results import OptimizationResults
+from .available_tickers import AvailableTickers
--- flask_api/src/utils/__init__.py
+++ flask_api/src/utils/__init__.py
@@ -1,1 +1,2 @@
 from .input_output import read_data_pgsql, dump_data_pgsql
+from .commons import load_and_merge_ymls
