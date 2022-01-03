53374a3ab0ef09de83fe5466f34b54a272b63e42
Guilherme Palazzo <35868223+gpalazzo@users.noreply.github.com>
2022-01-03 12:25:42 -0300

    How to use API (#41)
    * adding how to use the API tutorial
    
    * adding api usage example

--- flask_api/src/test_optimization_results_call.py
+++ flask_api/src/test_optimization_results_call.py
@@ -1,13 +1,7 @@
 import requests
-import sys
 
 
-host = str(sys.argv[1])
-if host == "remote":
-    url = "http://18.230.21.6:5000/optimization_results"
-else:
-    url = "http://localhost:5000/optimization_results"
-
+url = "http://localhost:5000/optimization_results"
 tickers = {"uuid": "bd54469a-9a25-4a31-a4f0-ca3f4c906899"}
 
 post_response = requests.post(url, json=tickers)
--- flask_api/src/test_portfolio_optimizer_call.py
+++ flask_api/src/test_portfolio_optimizer_call.py
@@ -1,13 +1,7 @@
 import requests
-import sys
 
 
-host = str(sys.argv[1])
-if host == "remote":
-    url = "http://18.230.21.6:5000/portfolio_optimize"
-else:
-    url = "http://localhost:5000/portfolio_optimize"
-
+url = "http://localhost:5000/portfolio_optimize"
 tickers = {"tickers": "AMER3.SA, ANIM3.SA, APER3.SA"}
 
 post_response = requests.post(url, json=tickers)
