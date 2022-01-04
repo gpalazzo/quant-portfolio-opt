d39ba39721ba4f8849a246df6d63b4ec1c80e8c4
Guilherme Palazzo <35868223+gpalazzo@users.noreply.github.com>
2022-01-04 18:12:06 -0300

    Adding API docs (#46)
    * api docs
    
    * adding md file for api

--- flask_api/src/resources/available_tickers.py
+++ flask_api/src/resources/available_tickers.py
@@ -9,14 +9,23 @@ from pathlib import Path
 # get the path to the root project directory
 project_dir = Path(__file__).resolve().parents[2]
 
-
+# reading configs from yml file
 CONFIG_PATH = [f"{os.getenv('PROJECT_ROOT_PATH')}/conf/io.yml"]
 
 
 class AvailableTickers(Resource):
     @staticmethod
-    @swag_from(f"{project_dir}/conf/endpoints/available_tickers.yml")
+    @swag_from(
+        f"{project_dir}/conf/endpoints/available_tickers.yml"
+    )  # parse swagger docs from file
     def get() -> Dict[str, List[str]]:
+        """Invoked when endpoint receives HTTP GET method
+            Parses available tickers from Airflow metadata table
+
+        Returns:
+            dict of str and list of available tickers
+        """
+
         config = load_and_merge_ymls(paths=CONFIG_PATH)
 
         df = read_data_pgsql(
@@ -24,6 +33,8 @@ class AvailableTickers(Resource):
             tbl_name=config["yf_raw_stock_metadata_tbl_name"],
         )
 
+        # gets only successfully dumped tickers
+        # this info comes from airflow metadata
         df = df[df["dump_status"] == "dumped"]
 
         return {"available_stocks": df["stock_names"].unique().tolist()}
--- flask_api/src/utils/input_output.py
+++ flask_api/src/utils/input_output.py
@@ -10,10 +10,23 @@ def read_data_pgsql(
     query: str = None,
     filter_params: List[Any] = None,
 ) -> pd.DataFrame:
+    """Read data from a PostgreSQL database
+
+    Args:
+        database: str of database name
+        tbl_name: str of table name
+        query: str of SQL query
+        filter_params: list of parameters to be filtered. Specially useful
+            by combining `where` and `in` clauses
+
+    Returns:
+        pandas dataframe with data from either entire table or query
+    """
 
     if not database:
         raise Exception("You must provide a database")
 
+    # build sql engine from environment variables
     print(f"Reading data from PGSQL")
     engine = create_engine(
         f"postgresql://{os.getenv('PGSQL_USERNAME')}:{os.getenv('PGSQL_PASSWORD')}@{os.getenv('PGSQL_HOST')}:{os.getenv('PGSQL_PORT')}/{database}"
@@ -23,6 +36,7 @@ def read_data_pgsql(
         raise Exception("Provide either a table name or a query")
 
     if not query:
+        # table name must be within double quotes ""
         df = pd.read_sql_query(
             f'select * from {database}.public."{tbl_name}"', con=engine
         )
@@ -30,8 +44,10 @@ def read_data_pgsql(
     else:
         if not filter_params:
             df = pd.read_sql_query(query, con=engine)
+
         else:
             cursor = engine.raw_connection().cursor()
+            # execute query by dynamically adding parameters
             cursor.execute(query, filter_params)
             df = pd.DataFrame(cursor.fetchall())
             df.columns = cursor.keys()
@@ -45,10 +61,22 @@ def dump_data_pgsql(
     tbl_name: str = None,
     append_data: bool = True,
 ) -> None:
+    """Dumps data to a PostgreSQL table
+
+    Args:
+        df: pandas dataframe holding data to be dumped
+        database: str of database name
+        tbl_name: str of table name
+        append_data: boolean to replace or append data to table
+
+    Returns:
+        None, it dumps data to a PostgreSQL table instead
+    """
 
     if not database or not tbl_name:
         raise Exception("You must provide a database and a table name to dump data")
 
+    # build sql engine from environment variables
     print(f"Dumping data to PGSQL")
     engine = create_engine(
         f"postgresql://{os.getenv('PGSQL_USERNAME')}:{os.getenv('PGSQL_PASSWORD')}@{os.getenv('PGSQL_HOST')}:{os.getenv('PGSQL_PORT')}/{database}"
@@ -61,6 +89,7 @@ def dump_data_pgsql(
         else:
             df.to_sql(tbl_name, engine, if_exists="replace", index=False)
 
+    # dummy error args printing for debugging purposes
     except Exception as e:
         print("Erro")
         print(e.args)
